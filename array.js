8b16105d5ff4d485e736f2b40689733a6ba06aaa
Emi0304 <sadiaparvin0304@gmail.com>
2023-03-25 00:25:37 +0600

    first commit

---
+++ array.js
@@ -0,0 +1,21 @@
+var age = [19, 30, 25, 35];
+console.log(age);
+
+// push 
+age.push(28);
+console.log(age);
+
+// pop 
+age.pop();
+console.log(age);
+
+// shift (remove the first value)
+age.shift();
+console.log(age);
+
+// unshift (add a first value)
+age.unshift(28);
+console.log(age);
+
+// map 
+age.map((age) => console.log(age));
---
+++ calculate.js
@@ -0,0 +1,58 @@
+var onionPrice = 50;
+var biscuitPrice = 20;
+
+
+let totalPrice = onionPrice + biscuitPrice;
+console.log("Total price is : " , totalPrice);
+
+var givenMoney = 500 ;
+var back = givenMoney - totalPrice;
+console.log("Back money :" , back)
+
+
+var number1 = 50;
+var number2 = 5;
+
+// added
+let resultForAdd = number1 + number2;
+console.log(resultForAdd);
+
+// minus 
+let resultForMinus = number1 - number2;
+console.log(resultForMinus);
+
+// count 
+let resultForCount= number1 * number2;
+console.log(resultForCount);
+
+// share 
+let resultForShare = number1 / number2;
+console.log(resultForShare);
+
+// add two string
+var firstName = "sadia";
+var secondName = "Emi";
+let name = firstName + " " + secondName;
+console.log(name);
+
+// add string 
+var string1 = "20";
+var string2 = "30";
+var stringAdd = string1 + string2;
+console.log(stringAdd);
+
+// parseInt 
+var number3 = "50";
+var number4 = "10";
+let result = parseInt(number3) + parseInt(number4);
+console.log(result);
+
+let result1 = parseInt(33.55) + parseInt(10.22);
+console.log(result1);
+
+let result2 = parseInt(65.89) + parseInt(38.64);
+console.log(result2);
+
+// parseFloat 
+let result3 = parseFloat(33.55) + parseFloat(10.22);
+console.log(result3);
